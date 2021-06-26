6bfecccbd7919313d3ae49d8676792819e87921e
bullet-ant <aman.kr.2129@gmail.com>
2021-06-26 19:24:20 +0530

    Added event binding

--- my-angular-app/src/app/servers/servers.component.ts
+++ my-angular-app/src/app/servers/servers.component.ts
@@ -2,14 +2,29 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
+  // selector: '[app-servers]',
+  // selector: '.app-servers',
   templateUrl: './servers.component.html',
+  // template: `
+  //   <app-server></app-server>
+  // `,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  addServer = false;
 
-  ngOnInit(): void {
+  addServerMessage = "No server added yet!";
+
+  constructor() {
+    setTimeout(() => {
+      this.addServer = true;
+    }, 3000);
   }
 
+  ngOnInit(): void { }
+
+  onAddServer() {
+    this.addServerMessage = "Yayy! Added a server!";
+  }
 }
---
+++ my-angular-app/src/app/warning/warning.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "app-warning",
+    templateUrl: "./warning.component.html",
+    styles: [`
+    div {
+        color: red;
+    }
+    `]
+})
+export class WarningComponent { }
